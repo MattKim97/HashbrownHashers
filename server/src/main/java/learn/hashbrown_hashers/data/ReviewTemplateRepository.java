d4c3cd5fbf10c506ecafa180a8413784cbc5aa17
nmichuda <nmichuda@gmail.com>
2024-08-05 16:16:46 -0400

    tag and review repo tests

--- server/src/main/java/learn/hashbrown_hashers/data/ReviewTemplateRepository.java
+++ server/src/main/java/learn/hashbrown_hashers/data/ReviewTemplateRepository.java
@@ -3,10 +3,17 @@ package learn.hashbrown_hashers.data;
 import learn.hashbrown_hashers.data.mappers.ReviewMapper;
 import learn.hashbrown_hashers.models.Review;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
-public class ReviewTemplateRepository implements ReviewRepository {
+
+@Repository
+public class ReviewTemplateRepository implements ReviewRepository{
     private final JdbcTemplate jdbcTemplate;
 
     public ReviewTemplateRepository(JdbcTemplate jdbcTemplate) {
@@ -16,7 +23,7 @@ public class ReviewTemplateRepository implements ReviewRepository {
 
     @Override
     public List<Review> findAll() {
-        final String sql = "select review, user_id, recipe_id, review_title, review_desc, rating "
+        final String sql = "select review_id, user_id, recipe_id, review_title, review_desc, rating "
                 + "from reviews limit 1000;";
         return jdbcTemplate.query(sql, new ReviewMapper());
 
@@ -24,7 +31,7 @@ public class ReviewTemplateRepository implements ReviewRepository {
 
     @Override
     public List<Review> findByRecipeId(int recipeId) {
-        final String sql = "select review, user_id, recipe_id, review_title, review_desc, rating "
+        final String sql = "select review_id, user_id, recipe_id, review_title, review_desc, rating "
                 + "from reviews "
                 + "where recipe_id = ?;";
         return jdbcTemplate.query(sql, new ReviewMapper(),recipeId);
@@ -32,10 +39,42 @@ public class ReviewTemplateRepository implements ReviewRepository {
 
     @Override
     public List<Review> findByUserId(int userId) {
-        final String sql = "select review, user_id, recipe_id, review_title, review_desc, rating "
+        final String sql = "select review_id, user_id, recipe_id, review_title, review_desc, rating "
                 + "from reviews "
                 + "where user_id = ?;";
         return jdbcTemplate.query(sql, new ReviewMapper(),userId);
+    }
+
+
+    @Override
+    public Review add(Review review){
+        final String sql = "insert into reviews (user_id, recipe_id, review_title, review_desc, rating) "
+                + " values (?, ?, ?, ?, ?);";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, review.getUserId());
+            ps.setInt(2, review.getRecipeId());
+            ps.setString(3, review.getTitle());
+            ps.setString(4, review.getDescription());
+            ps.setInt(5, review.getRating());
+            return ps;
+        }, keyHolder);
+
+        if (rowsAffected <= 0) {
+            return null;
+        }
+
+        review.setReviewId(keyHolder.getKey().intValue());
+        return review;
+    }
+
+    @Override
+    public boolean deleteById(int reviewId){
+        return jdbcTemplate.update(
+                "delete from reviews where review_id = ?;",
+                reviewId) > 0;
     }
 }
     /*
