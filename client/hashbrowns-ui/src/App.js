27c1dee9ce6706eeca7b1dd713028b5ca0b03d56
nmichuda <nmichuda@gmail.com>
2024-08-06 14:57:49 -0400

    added home navbar and recipeform components

--- client/hashbrowns-ui/src/App.js
+++ client/hashbrowns-ui/src/App.js
@@ -1,9 +1,11 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./Home"
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <Router>
+      <NavBar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
       </Routes>
