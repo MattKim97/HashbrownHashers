3e2a5782004b0bc595b4b3894f7070adcfc708dd
nmichuda <nmichuda@gmail.com>
2024-08-06 13:00:09 -0400

    finished review and tag service

---
+++ server/src/main/java/learn/hashbrown_hashers/data/controllers/TagController.java
@@ -0,0 +1,54 @@
+package learn.hashbrown_hashers.data.controllers;
+
+
+import learn.hashbrown_hashers.controllers.ErrorResponse;
+import learn.hashbrown_hashers.domain.Result;
+import learn.hashbrown_hashers.domain.TagService;
+import learn.hashbrown_hashers.models.Tag;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/tags")
+public class TagController {
+    private final TagService service;
+
+
+    public TagController(TagService service) {
+        this.service = service;
+    }
+
+    @GetMapping("")
+    public List<Tag> findAll(){
+        return service.findAll();
+    }
+
+    @GetMapping("/{text}")
+    public List<Tag> findByText(@PathVariable String text){
+        return service.findByText(text);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody Tag tag){
+        Result<Tag> result = service.add(tag);
+        if(result.isSuccess()){
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
+    }
+
+
+    @DeleteMapping("/{tagId}")
+    public ResponseEntity<Void> deleteById(@PathVariable int tagId){
+        if(service.deleteById(tagId)){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
