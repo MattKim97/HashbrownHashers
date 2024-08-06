bded13056f7fa56729094e75bece43fc4d43236f
nmichuda <nmichuda@gmail.com>
2024-08-06 16:34:43 -0400

    moved controllers into correct folder

--- client/hashbrowns-ui/src/App.js
+++ client/hashbrowns-ui/src/App.js
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./components/Home"
 import NavBar from "./components/NavBar";
+import AllRecipesList from "./components/AllRecipesList";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <NavBar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/recipes" element={<AllRecipesList/>}/>
       </Routes>
     </Router>
   );
