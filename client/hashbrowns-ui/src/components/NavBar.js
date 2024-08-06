51e0ae494700c60bd0a3a0d9f9e5b422d6b012e4
nmichuda <nmichuda@gmail.com>
2024-08-06 15:34:28 -0400

    navbar created along with misc styling

--- client/hashbrowns-ui/src/components/NavBar.js
+++ client/hashbrowns-ui/src/components/NavBar.js
@@ -1,18 +1,25 @@
 import { Link } from "react-router-dom";
-
+import './NavBar.css';
 
 function NavBar(){
     return(
-        <>
-        <nav>
+        
+        <nav className="navbar navbar-light bg-light">
             <Link to={'/'}>Home</Link>
+            <form className="form-inline">
+                <input className="form-control mr-sm-2  searchForm" type="search" placeholder="Search Recipes" aria-label="Search"/>
+                <button className="searchBtn" type="submit">Search</button>
+            </form>
             <Link to={'/'}>Chef's Choice</Link>
             <Link to={'/'}>View Recipes</Link>
             <Link to={'/'}>Create New</Link>
             <Link to={'/'}>My Recipes</Link>
             <Link to={'/'}>Log In/Log Out</Link>
+
+            
+
         </nav>
-        </>
+        
     )
 }
 
