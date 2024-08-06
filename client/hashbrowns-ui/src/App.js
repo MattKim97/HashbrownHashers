cbcf5b7295c8c8bcb45936ceda7c53df5bcb14a4
mattk <mkimucsb@gmail.com>
2024-08-06 13:35:13 -0700

    tested s3bucket, files are hosted correctly, links are provided back, temporay button tester implemented

--- client/hashbrowns-ui/src/App.js
+++ client/hashbrowns-ui/src/App.js
@@ -1,6 +1,9 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./components/Home"
 import NavBar from "./components/NavBar";
+import AllRecipesList from "./components/AllRecipesList";
+import ViewRecipe from "./components/ViewRecipe";
+import AddRecipeForm from "./components/AddRecipeForm";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <NavBar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/recipe" element={<AllRecipesList/>}/>
+        <Route path="/recipe/:id" element={<ViewRecipe/>}/>
+        <Route path="/recipe/new" element={<AddRecipeForm/>}/>
       </Routes>
     </Router>
   );
