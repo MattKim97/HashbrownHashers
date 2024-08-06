327ecbb76b3731280d05a4a5753f1d5460bd713a
mattk <mkimucsb@gmail.com>
2024-08-06 13:02:44 -0700

    created s3 form

--- client/hashbrowns-ui/src/components/AddRecipeForm.js
+++ client/hashbrowns-ui/src/components/AddRecipeForm.js
@@ -1,25 +1,46 @@
-
-
-
-
-
-const RECIPE_DEFAULT = {
-    recipeName: '',
-    difficulty: 1,
-    spicyness: 1,
-    prepTime: 0,
-    imageUrl: '',
-    description: '',
-    text: '',
-    userID: 0
-}
-
-
-
-
-
-
 function addRecipeForm(){
+
+    const [file, setFile] = useState(null);
+    const handleFileChange = (e) => {
+      const file = e.target.files[0];
+      setFile(file);
+    };
+
+    const uploadFile = async () => {
+        const S3_BUCKET = S3_BUCKET_IMAGE;
+        const REGION = REGION;
+
+        AWS.config.update({
+          accessKeyId: S3_KEY,
+          secretAccessKey: S3_SECRET,
+        });
+        const s3 = new AWS.S3({
+          params: { Bucket: S3_BUCKET },
+          region: REGION,
+        });
+
+        const params = {
+          Bucket: S3_BUCKET,
+          Key: file.name,
+          Body: file,
+        };
+
+        var upload = s3
+          .putObject(params)
+          .on("httpUploadProgress", (evt) => {
+            console.log(
+              "Uploading " + parseInt((evt.loaded * 100) / evt.total) + "%"
+            );
+          })
+          .promise();
+
+        await upload.then((err, data) => {
+          console.log(err);
+          alert("File uploaded successfully.");
+        });
+      };
+
+
     return(
         <>
         </>
