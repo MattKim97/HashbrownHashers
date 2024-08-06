b61f7e0f484c3c635fc58098225479fbfa0a0125
nmichuda <nmichuda@gmail.com>
2024-08-06 15:52:18 -0400

    added search functionality to navbar

--- client/hashbrowns-ui/src/components/NavBar.js
+++ client/hashbrowns-ui/src/components/NavBar.js
@@ -1,14 +1,31 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import './NavBar.css';
+import { useState } from "react";
 
 function NavBar(){
+
+    const navigate = useNavigate();
+    const [search, setSearch] = useState("");
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();        
+        navigate(`/recipes/${search}`)
+        setSearch(''); 
+    }
+    const handleChange = (event) =>{
+        setSearch(event.target.value);
+    }
+
+
+
+
     return(
         
         <nav className="navbar navbar-light bg-light">
             <Link to={'/'}>Home</Link>
-            <form className="form-inline">
-                <input className="form-control mr-sm-2  searchForm" type="search" placeholder="Search Recipes" aria-label="Search"/>
-                <button className="searchBtn" type="submit">Search</button>
+            <form className="form-inline" onSubmit={handleSubmit}>
+                <input value={search} onChange={handleChange} className="form-control mr-sm-2  searchForm" type="search" placeholder="Search Recipes" aria-label="Search"/>
+                <button className="searchBtn" >Search</button>
             </form>
             <Link to={'/'}>Chef's Choice</Link>
             <Link to={'/'}>View Recipes</Link>
