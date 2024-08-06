20f52a310dc83a20e44bccb82c93441d7fe1eb82
nmichuda <nmichuda@gmail.com>
2024-08-06 14:58:31 -0400

    added new components

---
+++ client/hashbrowns-ui/src/Home.js
@@ -0,0 +1,16 @@
+
+
+
+function Home(){
+    return(
+        <>
+        <header>
+            <h1>Welcome to Has#Browns!</h1>
+        </header>
+       
+        </>
+    )
+}
+
+
+export default Home;
