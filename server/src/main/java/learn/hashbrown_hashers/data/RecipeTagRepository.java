4a3f87294f5268e9d1950186d6eca2915fc87ffd
Nandan <nandangouri17@gmail.com>
2024-08-07 00:33:45 -0400

    Added RecipeTag, RecipeTagService, RecipeTagMapper, RecipeTagRepo, and made sure that it is easy to get tags.

---
+++ server/src/main/java/learn/hashbrown_hashers/data/RecipeTagRepository.java
@@ -0,0 +1,15 @@
+package learn.hashbrown_hashers.data;
+
+import learn.hashbrown_hashers.models.RecipeTag;
+import learn.hashbrown_hashers.models.Tag;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface RecipeTagRepository {
+
+    List<RecipeTag> findbyRecipeId(int recipeId);
+
+
+    RecipeTag add(RecipeTag recipeTag);
+}
---
+++ server/src/main/java/learn/hashbrown_hashers/data/RecipeTagTemplateRepository.java
@@ -0,0 +1,52 @@
+package learn.hashbrown_hashers.data;
+
+import learn.hashbrown_hashers.data.mappers.RecipeTagMapper;
+import learn.hashbrown_hashers.models.RecipeTag;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+
+@Repository
+public class RecipeTagTemplateRepository implements RecipeTagRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public RecipeTagTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    @Override
+    public List<RecipeTag> findbyRecipeId(int recipeId) {
+        final String sql = "select rt.recipe_id, rt.tag_id, t.tag_name " +
+                "from recipe_tags rt inner join tags t on t.tag_id=rt.tag_id " +
+                "where rt.recipe_id=?;";
+        return jdbcTemplate.query(sql,new RecipeTagMapper(), recipeId);
+    }
+
+    @Override
+    @Transactional
+    public RecipeTag add(RecipeTag recipeTag) {
+        final String sql = "insert into recipe_tags " +
+                "values(?,?);";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, recipeTag.getTagId());
+            ps.setInt(2, recipeTag.getRecipeId());
+            return ps;
+        }, keyHolder);
+
+        if (rowsAffected <= 0){
+            return null;
+        }
+
+        return recipeTag;
+    }
+
+}
