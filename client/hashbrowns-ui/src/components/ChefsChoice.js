d287d29ed3dbdf11ad64efb370eab2f33198f1d3
nmichuda <nmichuda@gmail.com>
2024-08-09 07:51:34 -0400

    fixed up hover and styling for chefs choice

--- client/hashbrowns-ui/src/components/ChefsChoice.js
+++ client/hashbrowns-ui/src/components/ChefsChoice.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { useEffect, useState, useRef } from 'react';
+import React from "react";
+import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import './ChefsChoice.css';
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
+import "./ChefsChoice.css";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export default function ChefsChoice() {
   const [recipes, setRecipes] = useState([]);
@@ -13,14 +13,14 @@ export default function ChefsChoice() {
 
   useEffect(() => {
     fetch(url)
-      .then(response => {
+      .then((response) => {
         if (response.status === 200) {
           return response.json();
         } else {
           return Promise.reject(`Unexpected status code: ${response.status}`);
         }
       })
-      .then(data => setRecipes(data))
+      .then((data) => setRecipes(data))
       .catch(console.log);
   }, []);
 
@@ -28,11 +28,14 @@ export default function ChefsChoice() {
 
   const container = useRef();
 
-  useGSAP(() => {
-    gsap.to(".box", { rotation: "+=360", duration: 3 });
-  }, { scope: container });
+  useGSAP(
+    () => {
+      gsap.to(".box", { rotation: "+=360", duration: 3 });
+    },
+    { scope: container }
+  );
 
-  const recipeIds = recipes.map(recipe => recipe.recipeId);
+  const recipeIds = recipes.map((recipe) => recipe.recipeId);
   const randomIndex = recipeIds[Math.floor(Math.random() * recipeIds.length)];
 
   const handleClick = () => {
@@ -40,11 +43,23 @@ export default function ChefsChoice() {
   };
 
   return (
-    <div ref={container} className='chefButtonContainer'>
-      <div className='chefTitle'>The Chef's Choice</div>
-      <button className='chefsButton box' onClick={handleClick}>See what the chef is cookin'</button>
-      <div className='chefDescription'>
-      Feeling uninspired in the kitchen? Let our chef take the guesswork out of meal planning! Just hit the button above, and voilà—an exciting recipe will appear, tailored to spark your culinary creativity. Whether you're craving comfort or adventure, the chef's choice has got you covered. Embrace a new flavor adventure and discover what’s cooking today!      </div>
-    </div>
+    <section>
+      <div className="chefHeader">
+        <h2>The Chef's Choice</h2>
+      </div>
+      <div ref={container} className="chefButtonContainer">
+        <button className="chefsButton box" onClick={handleClick}>
+          See what the chef is cookin'
+        </button>
+      </div>
+      <div className="chefDescription">
+        Feeling uninspired in the kitchen? Let our chef take the guesswork out
+        of meal planning! Just hit the button above, and voilà—an exciting
+        recipe will appear, tailored to spark your culinary creativity. Whether
+        you're craving comfort or adventure, the chef's choice has got you
+        covered. Embrace a new flavor adventure and discover what’s cooking
+        today!
+      </div>
+    </section>
   );
 }
